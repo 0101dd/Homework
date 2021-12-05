f4bd848a68171b25991137b1ec4b77ebb8aeddf2
0101dd <aa09335665@gmail.com>
2021-12-05 20:47:48 +0800

    jq - footer

---
+++ 211103-css-puppystore/js.js
@@ -0,0 +1,20 @@
+// footer 標籤
+$('#account').on('click', function () {
+  $('.jq-account').toggle()
+})
+
+$('#products').on('click', function () {
+  $('.jq-products').toggle()
+})
+
+$('#company').on('click', function () {
+  $('.jq-company').toggle()
+})
+
+$('#info').on('click', function () {
+  $('.footer-nav-3row').toggle()
+})
+
+$('#hamburger').on('click', function () {
+  $('.head-nav').toggle()
+})
