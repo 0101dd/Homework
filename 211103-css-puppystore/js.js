f6ac71588c6e3983c7ef1ecbe0737a2d2514e1f1
0101dd <aa09335665@gmail.com>
2021-12-12 20:38:06 +0800

    輪播

--- 211103-css-puppystore/js.js
+++ 211103-css-puppystore/js.js
@@ -52,3 +52,53 @@ $('#dog').on('click', function () {
 $('#fish').on('click', function () {
   $('#fish').next('ul').toggle()
 })
+
+// 圈圈----------------------------------------
+$(function () {
+  let now = 0
+  const total = $('.five-circle .circle').length
+  $('.five-circle').css('width', total * 200)
+
+  $('#btn-circle-r').on('click', function () {
+    if (now < total - 1) {
+      now++
+      const move = now * 200
+      $('.five-circle').stop(true, true)
+      $('.five-circle').animate({ left: `-${move + 60}px` }, 1000)
+    }
+  })
+
+  $('#btn-circle-l').on('click', function () {
+    if (now > 0) {
+      now--
+      const move = now * 200
+      $('.five-circle').stop(true, true)
+      if (move == 0) {
+        $('.five-circle').animate({ left: `${move + 60}px` }, 1000)
+      } else {
+        $(".five-circle").animate({ left: `-${move - 60}px` }, 1000)
+      }
+    }
+  })
+})
+
+// 輪播圖-------------------------------------------
+// $(function autoPlay() {
+//   time = setInterval(function () {
+//     $('.slide-img img').eq(0).css({ "transform": "translateX(-600px)", "transition": "2s" })
+//     $('.slide-img img').eq(1).css({ "transform": "translateX(-600px)", "transition": "2s" })
+//   }, 4000)
+//   time2 = setInterval(function () {
+//     $('.slide-img img').eq(0).css({ "transform": "translateX(0px)", "transition": "2s" })
+//     $('.slide-img img').eq(1).css({ "transform": "translateX(0px)", "transition": "2s" })
+//   }, 10000)
+// })
+
+time = setInterval(function () {
+  $('.slide-img img').eq(0).css({ "transform": "translateX(-600px)", "transition": "2s" })
+  $('.slide-img img').eq(1).css({ "transform": "translateX(-600px)", "transition": "2s" })
+}, 4000)
+time2 = setInterval(function () {
+  $('.slide-img img').eq(0).css({ "transform": "translateX(0px)", "transition": "2s" })
+  $('.slide-img img').eq(1).css({ "transform": "translateX(0px)", "transition": "2s" })
+}, 10000)
