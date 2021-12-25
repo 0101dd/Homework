46f42e9bddef1218ac17263c7e57a7ca1ad728a8
0101dd <aa09335665@gmail.com>
2021-12-26 00:36:13 +0800

    按鈕輪播

--- 211103-css-puppystore/js.js
+++ 211103-css-puppystore/js.js
@@ -118,3 +118,119 @@ $(function () {
     slideshowSpeed: 5000
   })
 })
+
+// trending 輪播---------------------------
+$(function () {
+  let now = 0
+  const total = $('.all-trend-box .trend-8-box').length
+  $('.all-trend-box').css('width', total * 245)
+
+  $('#trend-btn-right').on('click', function () {
+    if (now < total - 1) {
+      now++
+      const move = now * 245
+      $('.all-trend-box').stop(true, true)
+      $('.all-trend-box').animate({ left: `-${move}px` }, 1000)
+    }
+  })
+
+  $('#trend-btn-left').on('click', function () {
+    if (now > 0) {
+      now--
+      const move = now * 245
+      $('.all-trend-box').stop(true, true)
+      if (move == 0) {
+        $('.all-trend-box').animate({ left: `-${move}px` }, 1000)
+      } else {
+        $('.all-trend-box').animate({ left: `-${move}px` }, 1000)
+      }
+    }
+  })
+})
+
+// special products 輪播---------------------------
+$(function () {
+  let now = 0
+  const total = $('.special-all-box .trend-8-box').length
+  $('.special-all-box').css('width', total * 230)
+
+  $('#special-btn-right').on('click', function () {
+    if (now < total - 1) {
+      now++
+      const move = now * 230
+      $('.special-all-box').stop(true, true)
+      $('.special-all-box').animate({ left: `-${move}px` }, 1000)
+    }
+  })
+
+  $('#special-btn-left').on('click', function () {
+    if (now > 0) {
+      now--
+      const move = now * 230
+      $('.special-all-box').stop(true, true)
+      if (move == 0) {
+        $('.special-all-box').animate({ left: `-${move}px` }, 1000)
+      } else {
+        $('.special-all-box').animate({ left: `-${move}px` }, 1000)
+      }
+    }
+  })
+})
+
+// blog-------------------------------------
+$(function () {
+  let now = 0
+  const total = $('.blog-move .blog_pk_yl').length
+  $('.blog-move').css('width', total * 470)
+
+  $('#blog-btn-right').on('click', function () {
+    if (now < total - 1) {
+      now++
+      const move = now * 470
+      $('.blog-move').stop(true, true)
+      $('.blog-move').animate({ left: `-${move}px` }, 1000)
+    }
+  })
+
+  $('#blog-btn-left').on('click', function () {
+    if (now > 0) {
+      now--
+      const move = now * 470
+      $('.blog-move').stop(true, true)
+      if (move == 0) {
+        $('.blog-move').animate({ left: `-${move}px` }, 1000)
+      } else {
+        $('.blog-move').animate({ left: `-${move}px` }, 1000)
+      }
+    }
+  })
+})
+
+// client-----------------------------------
+$(function () {
+  let now = 0
+  const total = $('.client-move .client-box').length
+  $('.client-move').css('width', total * 310)
+
+  $('#client-btn-right').on('click', function () {
+    if (now < total - 1) {
+      now++
+      const move = now * 310
+      $('.client-move').stop(true, true)
+      $('.client-move').animate({ left: `-${move}px` }, 1000)
+    }
+  })
+
+  $('#client-btn-left').on('click', function () {
+    if (now > 0) {
+      now--
+      const move = now * 310
+      $('.client-move').stop(true, true)
+      if (move == 0) {
+        $('.client-move').animate({ left: `-${move}px` }, 1000)
+      } else {
+        $('.client-move').animate({ left: `-${move}px` }, 1000)
+      }
+    }
+  })
+})
